5ecdcca50b50c25e1e9438817eb4b389e412d90a
Pavithrap7 <palanipavithra2019@gmail.com>
2026-02-17 11:22:14 +0000

    change firebase_config

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,9 +1,11 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-
-cred=credentials.Certificate("firebase_key.json")
+# This will point to the JSON created in GitHub Actions
+cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
+db = firestore.client()
 
-db=firestore.client()
