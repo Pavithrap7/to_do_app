e62f60d866a2873943e27e5774d88ad88f6ec2fe
Pavithrap7 <palanipavithra2019@gmail.com>
2026-02-17 09:36:43 +0000

    added test

---
+++ test/test_main.py
@@ -0,0 +1,9 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+client=TestClient(app)
+
+def test_register():
+    response=client.post("/register",json={"name":"test_user","mail_id"="test@gmail.com","password"="testpass"})
+    assert "message" in response.json()
