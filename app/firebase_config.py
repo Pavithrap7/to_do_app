e2e039324c775408f90feb6fcd83a68d86f103e4
Pavithrap7 <palanipavithra2019@gmail.com>
2026-02-19 08:08:15 +0000

    remove breakpoint

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -3,7 +3,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 def get_db():
-    breakpoint()
     if not firebase_admin._apps:
         cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
         if not cred_path:
