ccda10406f8de1d65f38efd433862497af9a93ff
Pavithrap7 <palanipavithra2019@gmail.com>
2026-02-23 11:26:46 +0000

    modify test

--- test/test_main.py
+++ test/test_main.py
@@ -32,4 +32,4 @@ def test_show_tasks():
      client.post("/create_task",json={"mail":"test@gmail.com","task_name":"test_task2","description":"this is about creating tasks"})
      response=client.get("show_tasks/test@gmail.com")
      task=response.json()
-     assert any(tasks["name"] for tasks in task)=="test_task2"
+     assert any(tasks["name"]=="test_task2" for tasks in task)
