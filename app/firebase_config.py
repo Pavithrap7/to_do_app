b2516b07c80b6be87c4666b31040301eb2561ac5
Pavithrap7 <palanipavithra2019@gmail.com>
2026-02-24 09:42:56 +0000

    com

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,13 +1,10 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json
 
 def get_db():
     if not firebase_admin._apps:
-        cred_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
-        if not cred_json:
-            raise Exception("FIREBASE_CREDENTIALS not set")
-        cred = credentials.Certificate(json.loads(cred_json))
+        cred_path = "app/firebase_key.json"  # directly read from file
+        cred = credentials.Certificate(cred_path)
         firebase_admin.initialize_app(cred)
     return firestore.client()
