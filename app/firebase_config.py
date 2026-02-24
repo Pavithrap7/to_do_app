e8bbd06a9d9b943ac8cca43b9bfdae74be7d3393
Pavithrap7 <palanipavithra2019@gmail.com>
2026-02-24 09:51:42 +0000

    commit

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,10 +1,21 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+import base64
 
 def get_db():
     if not firebase_admin._apps:
-        cred_path = "app/firebase_key.json"  # directly read from file
-        cred = credentials.Certificate(cred_path)
+        key_path = "app/firebase_key.json"
+
+        # Create key file from secret if it doesn't exist
+        if not os.path.exists(key_path):
+            secret_b64 = os.environ.get("FIREBASE_KEY_BASE64")
+            if not secret_b64:
+                raise Exception("FIREBASE_KEY_BASE64 not set")
+            with open(key_path, "wb") as f:
+                f.write(base64.b64decode(secret_b64))
+
+        cred = credentials.Certificate(key_path)
         firebase_admin.initialize_app(cred)
+
     return firestore.client()
