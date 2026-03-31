11f9ff45574b749b17e00339b6334d5435fc3b5f
Pavithrap7 <palanipavithra2019@gmail.com>
2026-03-31 11:05:35 +0000

    add terraform k8s

---
+++ terraform_project/eks/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "todo-eks"
+  cluster_version = "1.29"
+
+  vpc_id = "vpc-07c41864bc5cb5db1"
+
+  subnet_ids = [
+    "subnet-0212d84eb91026c21",
+    "subnet-029f34d1fc6fa53f1"
+  ]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.micro"]
+    }
+  }
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
